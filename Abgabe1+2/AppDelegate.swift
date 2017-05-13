8be2ffa5a39340db3d88ab6cd807e5418bef9258
Rolf Paetow <rpa@dndapps.com>
2017-05-13 11:02:24 +0200

    Initial Commit

---
+++ Abgabe1+2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Abgabe1+2
+//
+//  Created by Rolf Paetow on 13.05.17.
+//  Copyright © 2017 Rolf Paetow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Abgabe1+2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Abgabe1+2
+//
+//  Created by Rolf Paetow on 13.05.17.
+//  Copyright © 2017 Rolf Paetow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
