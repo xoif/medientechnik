a957505b15f764fd56abe37118db85dccd3d8e38
Rolf Paetow <rpa@dndapps.com>
2017-05-13 16:31:20 +0200

    fix image loading

--- Abgabe1+2/ViewController.swift
+++ Abgabe1+2/ViewController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 import SnapKit
-import Photos
+import AppKit
 
 class ViewController: NSViewController {
 
@@ -35,6 +35,11 @@ class ViewController: NSViewController {
         contentView.addSubview(pushButton)
         
         imgView.wantsLayer = true
+        imgView.imageScaling = .scaleProportionallyDown //preserve the aspect ratio when image is loaded into image views' frame
+        imgView.setContentCompressionResistancePriority(12, for: .horizontal)
+        imgView.setContentCompressionResistancePriority(12, for: .vertical)
+        //if compression resistance is set to its default level (which is 500), the window will only scale down upon the image views' image intrinsic size.
+        
         imgView.layer?.backgroundColor = NSColor.green.cgColor
         
         sizeLabel.string = "Bitte lade ein Bild"
@@ -90,16 +95,67 @@ extension ViewController {
         guard let image = NSImage(named:"Strand.jpg") else {return}
         sizeLabel.string = "Das Bild ist \(image.size.width) Pixel breit und \(image.size.height) Pixel hoch"
         
-        let imageManager = PHImageManager.defaultManager()
-        imageManager.requestImageDataForAsset(self.asset!, options: nil, resultHandler:{
-            (data, responseString, imageOriet, info) -> Void in
-            let imageData: NSData = data!
-            if let imageSource = CGImageSourceCreateWithData(imageData, nil) {
-                let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil)! as NSDictionary
-                
-                
-            }
-        })
-    }
+        let pixelRepresentation = image.pixelData()
+        imgView.image = image
+        imgView.setContentCompressionResistancePriority(12, for: .horizontal)
+        imgView.setContentCompressionResistancePriority(12, for: .vertical)
+        
+        }
+    
 }
 
+
+
+extension NSImage {
+    
+    func pixelData() -> [Pixel] {
+        let bmp = self.representations[0] as! NSBitmapImageRep
+        var data: UnsafeMutablePointer<UInt8> = bmp.bitmapData!
+        var r, g, b, a: UInt8
+        var pixels: [Pixel] = []
+        
+        for row in 0..<bmp.pixelsHigh {
+            for col in 0..<bmp.pixelsWide {
+                r = data.pointee
+                data = data.advanced(by: 1)
+                g = data.pointee
+                data = data.advanced(by: 1)
+                b = data.pointee
+                data = data.advanced(by: 1)
+                a = data.pointee
+                data = data.advanced(by: 1)
+                pixels.append(Pixel(r: r, g: g, b: b, a: a, row: row, col: col))
+            }
+        }
+        return pixels
+    }
+    
+}
+
+struct Pixel {
+    
+    var r: Float
+    var g: Float
+    var b: Float
+    var a: Float
+    var row: Int
+    var col: Int
+    
+    init(r: UInt8, g: UInt8, b: UInt8, a: UInt8, row: Int, col: Int) {
+        self.r = Float(r)
+        self.g = Float(g)
+        self.b = Float(b)
+        self.a = Float(a)
+        self.row = row
+        self.col = col
+    }
+    
+    var color: NSColor {
+        return NSColor(red: CGFloat(r/255.0), green: CGFloat(g/255.0), blue: CGFloat(b/255.0), alpha: CGFloat(a/255.0))
+    }
+    
+    var description: String {
+        return "RGBA(\(r), \(g), \(b), \(a))"
+    }
+    
+}
